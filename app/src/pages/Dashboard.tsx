1dd7c774a97c3ba8650ffd6fa2a53e0832d57ea8
John Mills <mills.jpw@gmail.com>
2026-05-06 16:08:42 -0500

    recent lists front end, closes #63

--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 import Layout from "../components/Layout";
 import UpcomingBirthdaysCard from "../components/UpcomingBirthdaysCard";
+import RecentListsCard from "../components/RecentListsCard";
 
 export default function Dashboard() {
   return (
     <Layout>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2">
         <UpcomingBirthdaysCard />
+        <RecentListsCard />
       </div>
     </Layout>
   );
